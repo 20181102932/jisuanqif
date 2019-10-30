6bdf6d8d424877ac26c4abd85a75fae0cc79cb33
20181102932 <908907990@qq.com>
2019-10-30 11:29:03 +0800

    final

--- jisuanqi/ViewController.swift
+++ jisuanqi/ViewController.swift
@@ -10,6 +10,55 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var tfnum1: UITextField!
+    
+    @IBOutlet weak var tfnum2: UITextField!
+    
+    @IBOutlet weak var tfresult: UITextField!
+    
+    @IBOutlet weak var tfop: UITextField!
+    
+    @IBAction func jia(_ sender: UIButton) {
+        tfop.text="+";
+    }
+    
+    @IBAction func jian(_ sender: UIButton) {
+         tfop.text="-";
+    }
+    
+    @IBAction func cheng(_ sender: UIButton) {
+         tfop.text="*";
+    }
+    
+    @IBAction func chu(_ sender: UIButton) {
+         tfop.text="/";
+    }
+    
+    @IBAction func jisuan(_ sender: UIButton) {
+    let a = Double(tfnum1.text!)!
+    let b = Double(tfnum2.text!)!
+        if(tfop.text=="+")
+        {
+            let c = Double(a+b)
+           tfresult.text = String(c)
+            
+        }
+        if(tfop.text=="-")
+        {
+            let c = Double(a-b)
+            tfresult.text = String(c)
+        }
+        if(tfop.text=="*")
+        {
+            let c = Double(a*b)
+            tfresult.text = String(c)
+        }
+        if(tfop.text=="/")
+        {
+            let c = Double(a/b)
+            tfresult.text = String(c)
+        }
+    }
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
